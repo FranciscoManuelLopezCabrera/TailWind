3affa02606624fb823d9aaae4006fce6fcf8e201
FranciscoManuelLopezCabrera <franciscomanuel.lopez@adaits.es>
2023-01-19 17:46:20 +0100

    casfcsdvDASVCS
    CFDSCDASC

--- Ejercicio2/tailwind.config.js
+++ Ejercicio2/tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        zooming: {
+          "0%, 100%": {
+            transform: "scale(1.5)",
+          },
+        },
+      },
+      animation: {
+        "zooming-letra": "zooming 1s",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
